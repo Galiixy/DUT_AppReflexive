6254ce476e41c59aedb40221e5b6201de5b1a0f9
Gaëlle Ferreira <gaelle_f@live.fr>
2020-05-07 10:36:02 +0200

    add classes

---
+++ Serveur/src/ServeurBRi.java
@@ -0,0 +1,16 @@
+import bri.ServiceAmateur;
+import bri.ServiceProgrammeur;
+
+public class ServeurBRi implements Runnable {
+
+	public ServeurBRi(int portProg, Class<?> class1) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
